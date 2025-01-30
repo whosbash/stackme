b29c78799c77eb6c96b794c52090f08228697585
Bruno Peixoto <brunolnetto@gmail.com>
2025-01-30 17:49:52 -0300

    debug: checkpoint for item menu

--- stackme.sh
+++ stackme.sh
@@ -9664,6 +9664,7 @@ define_menu_stacks_miscelaneous() {
   # Generate menu items and append them to the array
   for app in "${!stacks[@]}"; do
       item="$(build_menu_item "$stack (${stacks[$stack]})" "Deploy" "deploy_stack_handler $stack")"
+      debug "$item"
       items+=("$item")
   done
 
